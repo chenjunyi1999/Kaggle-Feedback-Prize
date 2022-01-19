b5eb0073e2a4f126f49441c54705219dca1f610c
chenjunyi <1162369186@qq.com>
2022-01-19 18:52:56 +0800

    1

--- tool/correctspelling.py
+++ tool/correctspelling.py
@@ -1,58 +1,27 @@
-# #! pip3 install pyspellchecker
+import nltk
+from nltk.corpus import wordnet
+import random
+import numpy as np
+nltk.download('wordnet')
 
-commas = ['.', ',', '!', '...', '......', '?', '"', '\'']
-
-
-
-def correct(words, type=0):
-    from spellchecker import SpellChecker
-    spell = SpellChecker()
-    # 是字符，也返回字符
-    if type == 0:
-        words = words.split(' ')
-        for i in range(len(words)):
-            cur_word = spell.correction(words[i])
-            if words[i] != cur_word:
-                if words[i][-1] in commas:
-                    if words[i][:-1] != spell.correction(words[i][:-1]):
-                        words[i] = spell.correction(words[i][:-1]) + words[i][-1]
-                    else:
-                        continue
-                else:
-                    words[i] = cur_word
-        return ' '.join(words)
-
-    # 是列表，也返回列表
-    elif type == 1:
-        for i in range(len(words)):
-            cur_word = spell.correction(words[i])
-            if words[i] != cur_word:
-                if words[i][-1] in commas:
-                    if words[i][:-1] != spell.correction(words[i][:-1]):
-                        words[i] = spell.correction(words[i][:-1]) + words[i][-1]
-                        print("===", words[i])
-                    else:
-                        continue
-                else:
-                    words[i] = cur_word
-        return words
-
-    # 类型错误
-    else:
-        print("type error")
-        return
-
-
-if __name__ == '__main__':
-    # 字符串测试
-    words = "If the studant asks both his friends and his doctoars, he is able to use his judgement skills to determina which choice will be best for him in the long run."
-    print(words)
-    words2 = correct(words)
-    print(words2)
-
-    # 列表测试
-    words = "If the studant asks both his friends and his doctoars, he is able to use his judgement skills to determina which choice will be best for him in the long run."
-    words = words.split(' ')
-    print(words)
-    words2 = correct(words, 1)
-    print(words2)
+# 有待测试
+def word_level_aug(text, ratio=0.5):
+    text = text.split()
+    length = len(text)
+    num_changes = int(length*ratio)
+    # 生成可能含有相同的
+    idxs = np.random.randint(0, length-1, size=num_changes)
+    # 生成肯定不相同的
+    idxs = random.sample(range(0, length-1), num_changes)
+    for idx in idxs:
+        if text[idx][-1] in ['\'','"','.',',','?','!','......','...']:
+            continue
+        idx_synonyms = []
+        for syn in wordnet.synsets(text[idx]):
+            for lm in syn.lemmas():
+                idx_synonyms.append(lm.name())
+        if len(idx_synonyms)<1:
+            continue
+        text[idx] = random.sample(idx_synonyms, 1)[0]
+    print(text)
+    return ' '.join(text)
