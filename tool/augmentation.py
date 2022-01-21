67e2533b13fbea3ae41ce21c44e7d8ed3bf44d64
ddf62 <zhangh136@126.com>
2022-01-21 12:50:34 +0800

    对两个逼类单独做augment，和原来的一起存成一个csv

--- tool/augmentation.py
+++ tool/augmentation.py
@@ -1,9 +1,14 @@
+import copy
+
 import nltk
 from nltk.corpus import wordnet
 import random
-import numpy as np
+import numpy as np, os
+import pandas as pd, gc
+from tqdm import tqdm
 #nltk.download('wordnet')
 
+random.seed(42)
 # 有待测试
 def word_level_aug(text, ratio=0.5):
     text = text.split()
@@ -54,10 +59,10 @@ def ri_rs_rd(text, label, ratio):
                     idx_synonyms.append(lm.name())
             if len(idx_synonyms)<1:
                 continue
-            index = random.randint(0, len(text))
+            index = random.randint(0, len(text) - 1)
             n = 0
             while k != label[index]:
-                index = random.randint(0, len(text))
+                index = random.randint(0, len(text) - 1)
                 if n > 10:
                     break
             text.insert(index, random.sample(idx_synonyms, 1)[0])
@@ -90,6 +95,57 @@ def ri_rs_rd(text, label, ratio):
 
     return ' '.join([i for i in text]), label
 
-print(ri_rs_rd('one person says to do.', ['B-a', 'I-a','I-a','B-2','I-2'], 0.5))
+
+
+# 读取训练集标签
+train_df = pd.read_csv('../input/feedback-prize-2021/train.csv')
+print(train_df.shape)
+train_names, train_texts = [], []
+for f in tqdm(list(os.listdir('../input/feedback-prize-2021/train'))):
+    train_names.append(f.replace('.txt', ''))
+    train_texts.append(open('../input/feedback-prize-2021/train/' + f, 'r').read())
+train_text_df = pd.DataFrame({'id': train_names, 'text': train_texts})
+print(train_text_df.head())
+
+num = 1
+new_id = []
+new_text = []
+new_entit = []
+all_entities = []
+
+for ii, i in enumerate(train_text_df.iterrows()):
+    if ii % 100 == 0:
+        print(ii, ', ', end='')
+    # i[0]为序号，真正的字典存储在i[1]
+    total = i[1]['text'].split().__len__()
+    entities = ["O"] * total
+    for j in train_df[train_df['id'] == i[1]['id']].iterrows():
+        discourse = j[1]['discourse_type']
+        list_ix = [int(x) for x in j[1]['predictionstring'].split(' ')]
+        entities[list_ix[0]] = f"B-{discourse}"
+        for k in list_ix[1:]: entities[k] = f"I-{discourse}"
+    all_entities.append(entities)
+    if 'I-Counterclaim' in entities or 'I-Rebuttal' in entities:
+        for jk in range(2):
+            text = copy.deepcopy(i[1]['text'])
+            entit = copy.deepcopy(entities)
+            radint = random.randrange(0, 10)
+            if radint < 5:
+                text = word_level_aug(text, 0.3)
+            else:
+                text, entit = ri_rs_rd(text, entit, 0.2)
+            new_id.append(num)
+            new_text.append(text)
+            new_entit.append(entit)
+            num += 1
+
+train_text_df['entities'] = all_entities
+print(len(train_text_df))
+for i in range(len(new_id)):
+    train_text_df.loc[len(train_text_df)] = [new_id[i], new_text[i], new_entit[i]]
+print(len(train_text_df))
+train_text_df.to_csv('../input/train_NER_augment_counter_and_rebuttal.csv', index=False)
+
+
 # todo 句子级别同义、增删、翻转
 
